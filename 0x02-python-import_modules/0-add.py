77b8a8ecaf57850a10ad33d61daf10b34f9f9403
Melusi2022 <melusimedia@gmail.com>
2023-11-02 12:09:55 -0700

    My first tool box

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if __name__ =="__main__":
+if __name__ == "__main__":
     import add_0 as plus
     a = 1
     b = 2
     c = plus.add(a, b)
-print("{} + {} = {}".format(a, b, c))
+    print("{} + {} = {}".format(a, b, c))

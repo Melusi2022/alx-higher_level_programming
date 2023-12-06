693efacc009b11d31b074dd9900f1dbf49db4662
Melusi2022 <melusimedia@gmail.com>
2023-12-06 03:18:54 -0800

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module to write an empty class
+"""
+
+
+class BaseGeometry:
+    """
+    Module to write an empty class
+    """
+    def area(self):
+        """
+        check area
+        """
+        raise Exception("area () is not implemented")

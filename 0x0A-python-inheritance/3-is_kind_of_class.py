667d66d198382be611fddf5b148e6cd1cb7a9854
Melusi2022 <melusimedia@gmail.com>
2023-12-06 01:50:44 -0800

    same class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+checks if of specified class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    check if class is under
+    """
+    return isinstance(obj, a_class)

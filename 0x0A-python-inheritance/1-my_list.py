1a264f3e36ed248715a81ba843d14acb5adb9d15
Melusi2022 <melusimedia@gmail.com>
2023-12-06 01:41:23 -0800

    My_list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+prints lists inherited
+"""
+
+
+class MyList(list):
+    """
+    child class of list
+    """
+    def print_sorted(self):
+        """
+        Module to sort a list
+        """
+        print(sorted(self.copy()))

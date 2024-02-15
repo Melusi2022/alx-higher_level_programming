6fbb6c80ff705d6562273aa3c865522fe219ce59
Melusi2022 <melusimedia@gmail.com>
2024-02-14 23:08:12 -0800

    Sorted occurences fix

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,3 +1,20 @@
 #!/usr/bin/node
 
 const dict = require('./101-data').dict;
+
+const occurrencesById = {};
+
+for (const userID in dict)
+{
+	const occurrences = dict[userId];
+	if (occurrences in occurrencesById)
+	{
+		occurrencesById[uccurrences].push(userId);
+	}
+	else
+	{
+		occurrencesById[occurrences] = [userId];
+	}
+}
+
+console.log(occurencesById);

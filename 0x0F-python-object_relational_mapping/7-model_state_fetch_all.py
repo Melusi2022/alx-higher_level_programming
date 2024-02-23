03281467a1d70c08d150097b34e9c4b66c664260
Melusi2022 <melusimedia@gmail.com>
2024-02-22 22:57:01 -0800

    Fix All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
-"""Print all states in database sorted
 """
-import sys
-from model_state import Base, State
+This script lists all State objects from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
 from sqlalchemy import create_engine
+from model_state import State
 from sqlalchemy.orm import sessionmaker
 
-
-def main():
-    """Print all states in database sorted
-    """
-    us = sys.argv[1]
-    pa = sys.argv[2]
-    db = sys.argv[3]
-
-    engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'.format(us, pa, db),
-            pool_pre_ping=True
-            )
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    states = session.query(State)
-    i = 1
-    for state in states:
-        print('{}: {}'.format(i, state.name))
-        i += 1
-
-
 if __name__ == "__main__":
-    main()
+    # Create database engine
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    # Create session object
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
+
+    # Query all State objects and print their ids and names
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))

e78c7059f5cbd18a3a961ec66b699211be97e426
Melusi2022 <melusimedia@gmail.com>
2024-02-22 23:04:17 -0800

    Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,35 +1,31 @@
 #!/usr/bin/python3
-"""Print states with a filter from argument in database
 """
-import sys
-from model_state import Base, State
+This script prints the State object with the name passed as
+argument from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
 from sqlalchemy import create_engine
+from model_state import State
 from sqlalchemy.orm import sessionmaker
 
-
-def main():
-    """Print states with a filter from argument in database
-    """
-    us = sys.argv[1]
-    pa = sys.argv[2]
-    db = sys.argv[3]
-    sn = sys.argv[4]
-
-    engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'.format(us, pa, db),
-            pool_pre_ping=True
-            )
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    statesFiltered = session.query(State).filter(State.name.like(sn)).all()
-    if statesFiltered:
-        for state in statesFiltered:
-            print(state.id)
-    else:
-        print('Not found')
-
-    session.close()
-
-
 if __name__ == "__main__":
-    main()
+    # Create engine object to connect to the database
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]))
+
+    # Create session maker object to interact with the database
+    session_maker = sessionmaker(bind=engine)
+
+    # Create session object to execute database queries
+    session = session_maker()
+
+    # Query the database for the state with the provided name
+    for state in session.query(State):
+        if (state.name == argv[4]):
+            # If found, print its ID and exit the loop
+            print(state.id)
+            break
+    else:
+        # If not found, print an error message
+        print("Not found")

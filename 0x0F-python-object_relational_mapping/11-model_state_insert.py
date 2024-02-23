f6ceb69d47d2e3f08b69ef72f751f99e2f90e18e
Melusi2022 <melusimedia@gmail.com>
2024-02-22 23:06:05 -0800

    Add a new state

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,38 +1,28 @@
 #!/usr/bin/python3
-"""Add new detail
 """
-import sys
-from model_state import Base, State
+This script adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
+
+from sys import argv
 from sqlalchemy import create_engine
+from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 
+if __name__ == "__main__":
+    # Create a database connection using the provided credentials
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]))
 
-def main():
-    """Add new detail
-    """
-    us = sys.argv[1]
-    pa = sys.argv[2]
-    db = sys.argv[3]
+    # Create a session object to interact with the database
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
 
-    engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'.format(us, pa, db),
-            pool_pre_ping=True
-            )
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    # Create a new State object and add it to the session
+    louisiana = State(name="Louisiana")
+    session.add(louisiana)
 
-    newState = State(name='Louisiana')
-    session.add(newState)
+    # Commit the changes to the database
     session.commit()
 
-    statesFiltered = session.query(State).filter(
-            State.name.like('Louisiana')).all()
-    if statesFiltered:
-        for state in statesFiltered:
-            print(state.id)
-
-    session.close()
-
-
-if __name__ == "__main__":
-    main()
+    # Print the ID of the newly created State object
+    print(louisiana.id)

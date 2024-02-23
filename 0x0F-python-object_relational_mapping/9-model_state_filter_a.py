b4f9b9239d851f323d865866c8eda9acf3e7afff
Melusi2022 <melusimedia@gmail.com>
2024-02-22 23:01:26 -0800

    Fix Contains 'a'

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
-"""Print states with a filter in database
 """
-import sys
-from model_state import Base, State
+This script lists all State objects that contain the
+letter a from the database hbtn_0e_6_usa.
+"""
+
+from sys import argv
 from sqlalchemy import create_engine
+from model_state import State
 from sqlalchemy.orm import sessionmaker
 
-
-def main():
-    """Print states with a filter in database
-    """
-    us = sys.argv[1]
-    pa = sys.argv[2]
-    db = sys.argv[3]
-
-    engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'.format(us, pa, db),
-            pool_pre_ping=True
-            )
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    statesFiltered = session.query(State).filter(State.name.like('%a%')).all()
-    if statesFiltered:
-        for state in statesFiltered:
-            print('{}: {}'.format(state.id, state.name))
-
-    session.close()
-
-
 if __name__ == "__main__":
-    main()
+    # Create an engine object to connect to the MySQL database
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]))
+
+    # Create a sessionmaker object to create sessions
+    session_maker = sessionmaker(bind=engine)
+
+    # Create a session object
+    session = session_maker()
+
+    # Query the database for all State objects and
+    # print the ones that contain the letter a
+    for state in session.query(State).order_by(State.id):
+        print(f"{state.id}: {state.name}") if 'a' in state.name else ""

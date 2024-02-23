f1fde76a8127d43afc7e383287ec7d473400caea
Melusi2022 <melusimedia@gmail.com>
2024-02-22 22:59:05 -0800

    First state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
-"""Print one state in database
 """
-import sys
-from model_state import Base, State
+This script prints the first state object from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
 from sqlalchemy import create_engine
+from model_state import State
 from sqlalchemy.orm import sessionmaker
 
-
-def main():
-    """Print one state in database
-    """
-    us = sys.argv[1]
-    pa = sys.argv[2]
-    db = sys.argv[3]
-
-    engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'.format(us, pa, db),
-            pool_pre_ping=True
-            )
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    firstState = session.query(State).first()
-    if firstState:
-        print('1: {}'.format(firstState.name))
-    else:
-        print('Nothing')
-
-    session.close()
-
-
 if __name__ == "__main__":
-    main()
+    # Create database  engine
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    # Create a new session object  which handle database connections
+    session_maker = sessionmaker(bind=engine)
+
+    # Create a new session object
+    session = session_maker()
+
+    # Query the State table for first state
+    state = session.query(State).order_by(State.id).first()
+
+    # Print the State object's id and name, or nothing if state is not found
+    print("Nothing" if state is None else f"{state.id}: {state.name}")

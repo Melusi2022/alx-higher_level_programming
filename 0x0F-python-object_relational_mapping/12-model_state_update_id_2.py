0ea31eeb23a2a0d8b3ad39fa431db7e6209007ae
Melusi2022 <melusimedia@gmail.com>
2024-02-22 23:09:21 -0800

    Update a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
-"""Update value in database
 """
-import sys
-from model_state import Base, State
+This script changes the name of a State object from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
 from sqlalchemy import create_engine
+from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 
-
-def main():
-    """Update value in database
-    """
-    us = sys.argv[1]
-    pa = sys.argv[2]
-    db = sys.argv[3]
-
-    engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'.format(us, pa, db),
-            pool_pre_ping=True
-            )
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    state = session.query(State).filter(State.id == 2).first()
-
-    state.name = 'New Mexico'
-
-    session.commit()
-    session.close()
-
-
 if __name__ == "__main__":
-    main()
+    # Connect to MySQL server with provided credentials
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]))
+
+    # Create session object
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
+
+    # Query the database for the State object with id=2
+    state = session.query(State).filter_by(id=2).first()
+
+    # Modify the State object's name
+    state.name = "New Mexico"
+
+    # Commit the changes to the database
+    session.commit()

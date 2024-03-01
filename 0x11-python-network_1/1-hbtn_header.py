f57df82d499f8ef92329f8d148466005c7381e80
Melusi2022 <melusimedia@gmail.com>
2024-03-01 06:01:55 -0800

    fix Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""Python script that:
-    - takes in a URL,
-    - sends a request to the URL and
-    - displays the value of the X-Request-Id variable
-    - found in the header ofthe response.
-    """
-    from sys import argv
-    from urllib.request import Request, urlopen
+"""Python script that fetches a variable from header
+"""
+import urllib.request
+import sys
 
-    if __name__ == "__main__":
-            url = argv[1]
 
-                request = Request(url)
-                    with urlopen(request) as response:
-                                print(dict(response.headers).get("X-Request-Id"))
+def main():
+        """Python script that fetches a variable from header
+            """
+                url = sys.argv[1]
+                    req = urllib.request.Request(url)
+
+                        with urllib.request.urlopen(req) as response:
+                                    content_val = response.info()['X-Request-Id']
+                                            print(content_val)
+
+
+                                            if __name__ == "__main__":
+                                                    main()

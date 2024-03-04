66483296da29f98e6dbfb0ead57a2242bf447a12
Melusi2022 <melusimedia@gmail.com>
2024-03-03 23:47:21 -0800

    POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
+"""takes in a URL and an email address, sends a POST request to the
+passed URL with the email as a parameter, and finally displays the body
+of the response.
+
+Eg: ./6-post_email.py <URL> <email>
 """
-Python script that:
-    - takes in a URL and an email address,
-    - sends a POST request to the passed URL with the email as a parameter,
-    - and finally displays the body of the response.
-    """
+import sys
+import requests
 
 
-    if __name__ == '__main__':
-            from sys import argv
-                from requests import post
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
 
-                    url = argv[1]
-                        email = argv[2]
-                            res = post(url, {'email': email})
-                                print(res.text)
+    r = requests.post(url, data=value)
+    print(r.text)
